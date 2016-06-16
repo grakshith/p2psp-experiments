47c3baf695df1aa24521005029d420bd02d44d82
hehaichi <hehaichi@gmail.com>
2016-06-16 22:55:08 +0530

    Sleep between launch of two splitters

--- stat/run.sh
+++ stat/run.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 ./../bin/splitter --NTS --source_addr 150.214.150.68 --source_port 4551 --channel BBB-134.ogv > /dev/null &
+sleep 3
 ./../bin/splitter --NTS --source_addr 150.214.150.68 --source_port 4551 --team_port 8002 --channel BBB-134.ogv > /dev/null &
 ./../bin/peer --monitor --splitter_addr 127.0.0.1 --splitter_port 8001 > /dev/null &
-./../peer --monitor --splitter_addr 127.0.0.1 --splitter_port 8002 --player_port 10000 > /dev/null &
+./../bin/peer --monitor --splitter_addr 127.0.0.1 --splitter_port 8002 --player_port 10000 > /dev/null &
 vlcold http://localhost:9999 &
 vlcold http://localhost:10000 &
