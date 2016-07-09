ebcd28735159bfa05b218c535fcb58633dad8ebd
hehaichi <hehaichi@gmail.com>
2016-07-09 23:39:57 +0530

    Added diagnostic messages

--- src/synchronizer.cc
+++ src/synchronizer.cc
@@ -28,7 +28,7 @@ namespace p2psp {
       boost::program_options::options_description desc("This is the synchronizer node of P2PSP.\n");
       desc.add_options()
       ("help", "Produce this help message and exit.")
-      ("peers",boost::program_options::value<std::vector<std::string> >(),"Peers list");
+      ("peers",boost::program_options::value<std::vector<std::string> >()-> multitoken(),"Peers list");
       boost::program_options::variables_map vm;
       try{
       boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -48,8 +48,10 @@ namespace p2psp {
       if(vm.count("peers"))
       {
         peer_list = &vm["peers"].as<std::vector<std::string> >();
+        TRACE(peer_list->size()<< " peers passed to the synchronizer");
         // Run the RunThreads function which in turn starts the threads which connect to the peers
-        boost::thread(&Synchronizer::RunThreads,this);
+        boost::thread t1(&Synchronizer::RunThreads,this);
+        t1.join();
       }
     }
 
@@ -58,14 +60,17 @@ namespace p2psp {
       // Iterarate through the peer_list and start a thread to connect to the peer for every peer in peer_list
       for(std::vector<std::string>::const_iterator it = peer_list->begin();it!=peer_list->end();++it)
       {
+        TRACE("Running thread " << it-peer_list->begin()+1 << " of " << peer_list->end() - peer_list->begin() );
         thread_group_.interrupt_all();
         thread_group_.add_thread(new boost::thread(&Synchronizer::ConnectToPeers,this,*it,(it-peer_list->begin())));
-        thread_group_.join_all(); //Wait for all threads to complete
+
       }
+      thread_group_.join_all(); //Wait for all threads to complete
     }
 
     void Synchronizer::ConnectToPeers(std::string s, int id) throw(boost::system::system_error)
     {
+        TRACE("Connecting to " << s);
         std::vector<std::string> fields;
         boost::algorithm::split(fields,s,boost::is_any_of(":"));
         const boost::asio::ip::address hs = boost::asio::ip::address::from_string(fields[0]);
@@ -73,9 +78,11 @@ namespace p2psp {
         boost::asio::ip::tcp::endpoint peer(hs,port);
         boost::asio::ip::tcp::socket peer_socket (io_service_);
         peer_socket.connect(peer);
+        TRACE("Connected to "<< s);
         peer_data[id].resize(1024);
         while(1)
         {
+        TRACE("Receiving data from "<< s);
         boost::asio::read(peer_socket,boost::asio::buffer(peer_data[id]));
         if(synchronized)
         {
@@ -93,6 +100,7 @@ namespace p2psp {
           string. Once we find the offset, we trim the corresponding peer_data vector according to the offset, so that we achieve
           synchronization. Synchronization is a one time process.
         */
+        TRACE("Attempting to synchronize peers");
         int start_offset=100,offset=6;
         std::string needle(peer_data[0].begin()+start_offset,peer_data[0].begin()+start_offset+offset);
         for(std::vector<std::vector<char> >::iterator it = peer_data.begin()+1; it!=peer_data.end();++it) //Iterating through all the elements of peer_data vector
@@ -106,7 +114,11 @@ namespace p2psp {
                 found_before=found; //We stop the loop when the found variable no more changes
             }
             if(found == std::string::npos) //If the string matching fails, continue
+            {
+            TRACE("Synchronization of peer "<< it-peer_data.begin()<<" with peer 0 failed\nIgnoring the peer");
             continue;
+            }
+            TRACE("Synchronized peer " << it-peer_data.begin());
             it->erase(it->begin(),it->begin()+found); //Trim the first 'found' bytes of the vector
         }
     }
