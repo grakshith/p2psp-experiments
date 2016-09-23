ff53be0b1fa9a1d7772150df55f89ed9de9e5a85
hehaichi <hehaichi@gmail.com>
2016-09-24 00:05:49 +0530

    Update script

--- run.sh
+++ run.sh
@@ -4,22 +4,21 @@ if [ ! -f bin/splitter ] || [ ! -f bin/monitor ] || [ ! -f bin/synchronizer ]
 	then
 	./make.py
 fi
-./run_a_source.sh 
+./run_a_source.sh
 sleep 2
 echo "Started source"
 echo "Starting splitter"
-bin/splitter --source_port 8080 --channel test.ts  &
-bin/splitter --source_port 8080 --channel test.ts --splitter_port 8002 &
+bin/splitter --source_port 8080 --channel test.ts >/dev/null 2>&1 &
+bin/splitter --source_port 8080 --channel test.ts --splitter_port 8002 >/dev/null 2>&1 &
 echo "Starting monitor"
-bin/monitor --splitter_port 8001  &
-bin/monitor --splitter_port 8002 --player_port 10000   &
+bin/monitor --splitter_port 8001  >/dev/null 2>&1 &
+bin/monitor --splitter_port 8002 --player_port 10000   >/dev/null 2>&1 &
 sleep 2
 echo "Starting synchronizer"
 bin/synchronizer --peers 127.0.0.1:9999 127.0.0.1:10000 &
-PID=$!
 sleep 5
 vlc http://localhost:15000 >/dev/null 2>&1 &
-fg $PID
 echo "PID $$ to kill this script"
 PID=$!
 wait $PID
+kill $(jobs -p)
